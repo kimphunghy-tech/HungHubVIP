69af452361ac3403629247852a3b3a73f6941ec5
kimphunghy-tech <kimphung.hy@gmail.com>
2026-02-01 13:11:13 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,36 @@
-print("HungHubVIP loaded!") 
+-- HƯNG HUB VIP UI TEST
+
+local CoreGui = game:GetService("CoreGui")
+
+pcall(function()
+    if CoreGui:FindFirstChild("HungHubVIP") then
+        CoreGui.HungHubVIP:Destroy()
+    end
+end)
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "HungHubVIP"
+ScreenGui.ResetOnSpawn = false
+ScreenGui.Parent = CoreGui
+
+local Frame = Instance.new("Frame")
+Frame.Parent = ScreenGui
+Frame.Size = UDim2.new(0, 320, 0, 180)
+Frame.Position = UDim2.new(0.5, -160, 0.5, -90)
+Frame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Frame.BorderSizePixel = 0
+
+local UICorner = Instance.new("UICorner")
+UICorner.CornerRadius = UDim.new(0, 20)
+UICorner.Parent = Frame
+
+local Title = Instance.new("TextLabel")
+Title.Parent = Frame
+Title.Size = UDim2.new(1, -20, 1, -20)
+Title.Position = UDim2.new(0, 10, 0, 10)
+Title.BackgroundTransparency = 1
+Title.TextWrapped = true
+Title.TextScaled = true
+Title.Font = Enum.Font.GothamBold
+Title.TextColor3 = Color3.fromRGB(255, 215, 0)
+Title.Text = "HƯNG HUB [⭐ VIP 👑]\n\nLOAD THÀNH CÔNG 🎉"
